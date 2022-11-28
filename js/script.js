dda01475e2c91542653e57abd92dc685d3cd7365
info-mic-info <jobsocial94@gmail.com>
2022-11-28 23:45:21 +0100

    css

--- js/script.js
+++ js/script.js
@@ -3,15 +3,10 @@
 // Scrivere un programma che chieda all’utente: - Il numero di chilometri da percorrere - Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole: - il prezzo del biglietto è definito in base ai km (0.21 € al km) - va applicato uno sconto del 20% per i minorenni - va applicato uno sconto del 40% per gli over 65.
 
 // 1) chiedere il numero di chilometri da percorrere
-let km = parseInt(prompt("Km che si desidera percorrere"));
+
 // 2) chiedere l'età del passeggero
 
-let eta = parseInt(prompt("Età del passeggero"));
-
 // 3) calcolare prezzo totale del biglietto in base ai km
-
-let price_tot = (km * 0.21)
-let price_final
 
 // 4) cacolare scontistica
 //    4.1) SE la persona è minorenne allora 20% di sconto
@@ -19,29 +14,62 @@ let price_final
 //    ALTRIMENTI prezzo normale del biglietto
 
 
-if (eta <= 18) {
-    price_final = (price_tot - (price_tot * 20 / 100)).toFixed(2);
+let bottone = document.getElementById("genera")
 
-}
+bottone.addEventListener("click", function () {
 
+    let eta = document.getElementById('eta').value
+    let km = document.getElementById('km').value
 
-else if (eta >= 65) {
-    price_final = (price_tot - (price_tot * 40 / 100)).toFixed(2);
-}
+    console.log('click', eta, km)
 
-else {
-    price_final=price_tot
-}
+    let price_tot = (km * 0.21)
+    let price_final
 
-console.log(price_final)
+    let minorenne
 
 
 
-const bottone = document.getElementById("genera")
+    if (eta <= 18  || eta === "minorenne") {
+        price_final = (price_tot - (price_tot * 20 / 100)).toFixed(2);
+        
+    }
 
-bottone.addEventListener()
+
+    else if (eta >= 65 || eta === "over-65") {
+        price_final = (price_tot - (price_tot * 40 / 100)).toFixed(2);
+    }
+
+    else {
+        price_final = price_tot
+    }
+
+    
+
+    console.log(price_final)
+
+    document.getElementById("price").innerHTML = price_final;
+
+})
 
 
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
